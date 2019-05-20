34a63fd39ddc631fb58fcd0cbfc329427995cc73
Mel Deraldo <melina.deraldo@ebanx.com>
2019-05-20 14:34:19 -0300

    Add some echos to the script

--- .install_ohmyzsh.sh
+++ .install_ohmyzsh.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 echo "Install oh my zsh"
-git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Change shell to be zsh"
 chsh -s /bin/zsh
 
-echo "Install zsh-autosuggestions & zsh-syntax-highlighting plugins"
+echo "Install zsh-autosuggestions & zsh-syntax-highlighting & zsh-completitions plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-echo "Install powerlevel9k theme"
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
+
+echo "Install powerlevel9k theme"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
